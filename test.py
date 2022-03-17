d7e96f26de5745bb79f462eda5804e1d4112dc7a
Sylwia0506 <sysazeszkoly@gmail.com>
2022-03-17 13:20:05 +0100

    add test PM2

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from algorytmy_impl import RF1, RF2, PM2, PM2_decrypt
+from algorytmy_impl import RF1, RF2, PM2, PM2_decrypt, PM, PM_decrypt
 
 
 class TestCrypt(unittest.TestCase):
@@ -18,16 +18,32 @@ class TestCrypt(unittest.TestCase):
         expected = "WEAREDISCOVEREDFLEEATONCE"
         self.assertEqual(actual, expected)
 
-    def test_checked_when_encrypt_matrix_conversion_is_correct(self):
+    def test_checked_when_encrypt_matrix_conversion1_is_correct(self):
         cipher = "CRYPTOGRAPHYOSA"
         key = "3-4-1-5-2"
         actual = PM2(cipher, key)
         expected = "RH PO OA R TS A CP YY G "
         self.assertEqual(actual, expected)
 
-    def test_checked_when_decrypt_matrix_conversion_is_correct(self):
+    def test_checked_when_decrypt_matrix_conversion1_is_correct(self):
         cipher = "RH PO OA R TS A CP YY G "
         key = "3-4-1-5-2"
         actual = PM2_decrypt(cipher, key)
         expected = "CRYPTOGRAPHYOSA"
+        self.assertEqual(actual, expected)
+
+    def test_checked_when_encrypt_matrix_conversion_is_correct(self):
+        cipher = "POLITECHNIKA"
+        d = 5
+        key = [3, 4, 1, 5, 2]
+        actual = PM(cipher, d, key)
+        expected = "LIPTOHNEICKA"
+        self.assertEqual(actual, expected)
+
+    def test_checked_when_decrypt_matrix_conversion_is_correct(self):
+        cipher = "LIPTOHNEICKA"
+        d = 5
+        key = [3, 4, 1, 5, 2]
+        actual = PM_decrypt(cipher, d, key)
+        expected = "POLITECHNIKA"
         self.assertEqual(actual, expected)
