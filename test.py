55a391d6f8313f804a516ff271414596b9559816
Sylwia0506 <sysazeszkoly@gmail.com>
2022-03-17 13:12:43 +0100

    add test PM

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from algorytmy_impl import RF1, RF2
+from algorytmy_impl import RF1, RF2, PM2, PM2_decrypt
 
 
 class TestCrypt(unittest.TestCase):
@@ -16,4 +16,18 @@ class TestCrypt(unittest.TestCase):
         key = 3
         actual = RF2(cipher, key)
         expected = "WEAREDISCOVEREDFLEEATONCE"
+        self.assertEqual(actual, expected)
+
+    def test_checked_when_encrypt_matrix_conversion_is_correct(self):
+        cipher = "CRYPTOGRAPHYOSA"
+        key = "3-4-1-5-2"
+        actual = PM2(cipher, key)
+        expected = "RH PO OA R TS A CP YY G "
+        self.assertEqual(actual, expected)
+
+    def test_checked_when_decrypt_matrix_conversion_is_correct(self):
+        cipher = "RH PO OA R TS A CP YY G "
+        key = "3-4-1-5-2"
+        actual = PM2_decrypt(cipher, key)
+        expected = "CRYPTOGRAPHYOSA"
         self.assertEqual(actual, expected)
