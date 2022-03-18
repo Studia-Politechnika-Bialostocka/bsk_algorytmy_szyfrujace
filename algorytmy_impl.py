530aee1eb8fba19f2c3f2634f2df5bfb1d04bd2c
WooooCash <lukaszbialczak3@gmail.com>
2022-03-18 21:29:52 +0100

    fixed PM and changed default key for PM2 to CONVENIENCE

--- algorytmy_impl.py
+++ algorytmy_impl.py
@@ -5,90 +5,86 @@ import math
 def PM(text, d=5, key=[3, 4, 1, 5, 2]):
     if len(text) <= 0:
         return "Nie wprowadzono tekstu"
-    else:
-        matrix = [["_" for i in range(d)] for j in range(math.ceil(len(text) / d))]
+    
+    matrix = [["_" for i in range(d)] for j in range(math.ceil(len(text) / d))]
 
-        row = 0
-        for i in range(len(text)):
-            matrix[row][i % d] = text[i]
-            if i % d == d - 1:
-                row += 1
+    row = 0
+    for i in range(len(text)):
+        matrix[row][i % d] = text[i]
+        if i % d == d - 1:
+            row += 1
 
-        encrypted = ""
+    encrypted = ""
+    row = 0
+    for i in range(len(matrix)):
+        for j in range(len(key)):
+            # if matrix[i][key[j] - 1] != "_":
+                encrypted += matrix[i][key[j] - 1]
 
-        row = 0
-        for i in range(len(matrix)):
-            for j in range(len(key)):
-                if matrix[i][key[j] - 1] != "_":
-                    encrypted += matrix[i][key[j] - 1]
-
-        return encrypted
+    return encrypted
 
 
 def PM_decrypt(text, d=5, key=[3, 4, 1, 5, 2]):
     if len(text) <= 0:
         return "Nie wprowadzono tekstu"
-    else:
-        matrix = [["_" for i in range(d)] for j in range(math.ceil(len(text) / d))]
+    
+    matrix = [["_" for i in range(d)] for j in range(math.ceil(len(text) / d))]
 
-        while len(text) % d != 0:
-            text += "_"
+    while len(text) % d != 0:
+        text += "_"
 
-        for i in range(math.ceil(len(text) / d)):
-            for j in range(d):
-                if i * d + j >= len(text):
-                    matrix[i][key[j] - 1] = "_"
-                else:
-                    matrix[i][key[j] - 1] = text[i * d + j]
+    for i in range(math.ceil(len(text) / d)):
+        for j in range(d):
+            matrix[i][key[j] - 1] = text[i * d + j]
 
-        matrix = np.array(matrix)
-        return "".join(matrix.flatten()).replace("_", "")
+    matrix = np.array(matrix)
+    return "".join(matrix.flatten()).replace("_", "")
 
 
 def PM2(text, key_word):
     if len(text) <= 0:
         return "Nie wprowadzono tekstu"
-    else:
-        d = len(key_word)
-        key = np.argsort(np.array(list(key_word)))
+    
+    d = len(key_word)
+    key = np.argsort(np.array(list(key_word)))
 
-        matrix = [["_" for i in range(d)] for j in range(math.ceil(len(text) / d))]
+    matrix = [["_" for i in range(d)] for j in range(math.ceil(len(text) / d))]
 
-        row = 0
-        for i in range(len(text)):
-            matrix[row][i % d] = text[i]
-            if i % d == d - 1:
-                row += 1
+    row = 0
+    for i in range(len(text)):
+        matrix[row][i % d] = text[i]
+        if i % d == d - 1:
+            row += 1
 
-        matrix = np.array(matrix)
-        matrix = matrix[:, key]
-        matrix = matrix.transpose()
+    matrix = np.array(matrix)
+    matrix = matrix[:, key]
+    matrix = matrix.transpose()
 
-        result = ""
-        for x in matrix:
-            result += "".join(x).replace("_", "")
-            result += " "
+    result = ""
+    for x in matrix:
+        result += "".join(x).replace("_", "")
+        result += " "
 
-        return result
+    return result
 
 
 def PM2_decrypt(text, key_word):
     if len(text) <= 0:
         return "Nie wprowadzono tekstu"
-    else:
-        d = len(key_word)
-        key = np.argsort(np.array(list(key_word)))
+    
+    d = len(key_word)
+    key = np.argsort(np.array(list(key_word)))
 
-        words = text.split()
-        max_col_len = max([len(x) for x in words])
-        matrix = [["_" for i in range(d)] for j in range(max_col_len)]
+    words = text.split()
+    max_col_len = max([len(x) for x in words])
+    matrix = [["_" for i in range(d)] for j in range(max_col_len)]
 
-        for i in range(len(key)):
-            for j in range(len(words[i])):
-                matrix[j][key[i]] = words[i][j]
+    for i in range(len(key)):
+        for j in range(len(words[i])):
+            matrix[j][key[i]] = words[i][j]
 
-        matrix = np.array(matrix)
-        return "".join(matrix.flatten()).replace("_", "")
+    matrix = np.array(matrix)
+    return "".join(matrix.flatten()).replace("_", "")
 
 
 def RF1(text, key):
