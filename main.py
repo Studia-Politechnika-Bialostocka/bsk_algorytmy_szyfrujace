0b7e6e67b2487e7b4ae6b5c393c2ef1b86be5715
mateuszbialowas <mateusz.bialowas2@gmail.com>
2022-04-06 13:25:56 +0200

    add rsa gui

--- main.py
+++ main.py
@@ -1,235 +1,67 @@
-import tkinter
-from tkinter import filedialog
 import pathlib
 import tkinter as tk
 import tkinter.ttk as ttk
+from tkinter import filedialog
+import pygubu
 
-from algorytmy_impl import PM, PM2, PM2_decrypt, PM_decrypt, RF1, RF2, random_generator_num, synchronous_stream_cipher
+from rsa_implementation import generate_public_private_key
 
 PROJECT_PATH = pathlib.Path(__file__).parent
-PROJECT_UI = PROJECT_PATH / "algorytmy_gui.ui"
+PROJECT_UI = PROJECT_PATH / "rsa_algorytm_gui.ui"
 
 
-class AlgorytmyGuiApp:
+class RsaAlgorytmGuiApp:
     def __init__(self, master=None):
-        # build ui
-        self.main_window = tk.Tk() if master is None else tk.Toplevel(master)
-        self.header_label = tk.Label(self.main_window)
-        self.header_label.configure(font='{Bahnschrift SemiLight SemiConde} 36 {bold}', text='Block algorithms')
-        self.header_label.pack(side='top')
-        self.frame7 = tk.Frame(self.main_window)
-        self.label1 = tk.Label(self.frame7)
-        self.label1.configure(font='{Bahnschrift SemiLight SemiConde} 16 {}', text='Input Code')
-        self.label1.pack(side='left')
-        self.code_input = ttk.Entry(self.frame7)
-        self.code_input.configure(width='65')
-        self.code_input.pack(anchor='center', fill='y', padx='10', side='left')
-        self.__tkvar = tk.StringVar(value='Encrypt')
-        __values = ['Decrypt']
-        self.algorithm_option = tk.OptionMenu(self.frame7, self.__tkvar, 'Encrypt', *__values, command=None)
-        self.algorithm_option.pack(fill='y', side='top')
-        self.frame7.configure(height='200', width='200')
-        self.frame7.pack(anchor='w', padx='20', pady='10', side='top')
-
-
-        # self.frame8 = tk.Frame(self.main_window)
-        # self.label2 = tk.Label(self.frame8)
-        # self.label2.configure(font='{Bahnschrift SemiLight SemiConde} 16 {}', text='Rail fence   ')
-        # self.label2.pack(side='left')
-        # self.label3 = tk.Label(self.frame8)
-        # self.label3.configure(text='number of levels (n)')
-        # self.label3.pack(side='left')
-        # self.rail_fence_n_input = tk.Spinbox(self.frame8)
-        # self.rail_fence_n_input.configure(from_='0', increment='1', justify='center', to='10')
-        # self.rail_fence_n_input.configure(width='2')
-        # self.rail_fence_n_input.delete(0, 'end')
-        # self.rail_fence_n_input.insert(0, '3')
-        # self.rail_fence_n_input.pack(fill='y', padx='10', side='left')
-        # self.rail_fence_run_button = tk.Button(self.frame8)
-        # self.rail_fence_run_button.configure(text='Run')
-        # self.rail_fence_run_button.pack(fill='y', ipadx='10', side='left')
-        # self.rail_fence_run_button.configure(command=self.rail_fence_run)
-        # self.frame8.configure(height='200', width='200')
-        # self.frame8.pack(anchor='w', padx='20', pady='10', side='top')
-        # self.frame9 = tk.Frame(self.main_window)
-        # self.label15 = tk.Label(self.frame9)
-        # self.label15.configure(font='{Bahnschrift SemiLight SemiConde} 16 {}', text='Matrix conversion a  ')
-        # self.label15.pack(side='left')
-        # self.label16 = tk.Label(self.frame9)
-        # self.label16.configure(text='coding key:')
-        # self.label16.pack(side='left')
-        # self.matrix_conversion_a_key_input = tk.Entry(self.frame9)
-        # self.matrix_conversion_a_key_input.configure(justify='center', width='15')
-        # self.matrix_conversion_a_key_input.delete(0, 'end')
-        # self.matrix_conversion_a_key_input.insert(0, '3-4-1-5-2')
-        # self.matrix_conversion_a_key_input.pack(fill='y', padx='10', side='left')
-        # self.label17 = tk.Label(self.frame9)
-        # self.label17.configure(text='d:')
-        # self.label17.pack(side='left')
-        # self.matrix_conversion_a_d_input = tk.Spinbox(self.frame9)
-        # self.matrix_conversion_a_d_input.configure(from_='0', increment='1', justify='center', to='10')
-        # self.matrix_conversion_a_d_input.configure(width='2')
-        # self.matrix_conversion_a_d_input.delete(0, 'end')
-        # self.matrix_conversion_a_d_input.insert(0, '5')
-        # self.matrix_conversion_a_d_input.pack(fill='y', padx='10', side='left')
-        # self.button6 = tk.Button(self.frame9)
-        # self.button6.configure(text='Run')
-        # self.button6.pack(fill='y', ipadx='10', side='left')
-        # self.button6.configure(command=self.matrix_conversion1_run)
-        # self.frame9.configure(height='200', width='200')
-        # self.frame9.pack(anchor='w', padx='20', pady='10', side='top')
-        # self.frame10 = tk.Frame(self.main_window)
-        # self.label21 = tk.Label(self.frame10)
-        # self.label21.configure(font='{Bahnschrift SemiLight SemiConde} 16 {}', text='Matrix conversion b  ')
-        # self.label21.pack(side='left')
-        # self.label22 = tk.Label(self.frame10)
-        # self.label22.configure(text='coding key:')
-        # self.label22.pack(side='left')
-        # self.matrix_conversion_b_key_input = tk.Entry(self.frame10)
-        # self.matrix_conversion_b_key_input.configure(justify='center', width='15')
-        # self.matrix_conversion_b_key_input.delete(0, 'end')
-        # self.matrix_conversion_b_key_input.insert(0, '3-4-1-5-2')
-        # self.matrix_conversion_b_key_input.pack(fill='y', padx='10', side='left')
-        # self.button8 = tk.Button(self.frame10)
-        # self.button8.configure(text='Run')
-        # self.button8.pack(fill='y', ipadx='10', side='left')
-        # self.button8.configure(command=self.matrix_conversion2_run)
-        # self.frame10.configure(height='200', width='200')
-        # self.frame10.pack(anchor='w', padx='20', pady='10', side='top')
-        # self.frame1 = tk.Frame(self.main_window)
-        # self.label4 = tk.Label(self.frame1)
-        # self.label4.configure(font='{Bahnschrift SemiLight SemiConde} 16 {}', text='Pseudo-random number generator')
-        # self.label4.pack(side='left')
-        # self.label5 = tk.Label(self.frame1)
-        # self.label5.configure(text='𝜑(𝑥)')
-        # self.label5.pack(side='left')
-        # self.entry1 = tk.Entry(self.frame1)
-        # self.entry1.configure(justify='center', width='15')
-        # self.entry1.delete(0, 'end')
-        # self.entry1.insert(0, '4-1')
-        # self.entry1.pack(fill='y', padx='10', side='left')
-        # self.button1 = tk.Button(self.frame1)
-        # self.button1.configure(text='Run')
-        # self.button1.pack(fill='y', ipadx='10', side='left')
-        # self.button1.configure(command=self.random_generator_run)
-        # self.frame1.configure(height='200', width='200')
-        # self.frame1.pack(anchor='w', padx='20', pady='10', side='top')
-        # self.stream_cipher_frame = tk.Frame(self.main_window)
-        # self.label6 = tk.Label(self.stream_cipher_frame)
-        # self.label6.configure(font='{Bahnschrift SemiLight SemiConde} 16 {}', text='Stream cipher')
-        # self.label6.pack(side='left')
-        # self.label7 = tk.Label(self.stream_cipher_frame)
-        # self.label7.configure(text='𝜑(𝑥)')
-        # self.label7.pack(side='left')
-        # self.stream_cipher_key = tk.Entry(self.stream_cipher_frame)
-        # self.stream_cipher_key.configure(justify='center', width='15')
-        # self.stream_cipher_key.delete(0, 'end')
-        # self.stream_cipher_key.insert(0, '4-1')
-        # self.stream_cipher_key.pack(fill='y', padx='10', side='left')
-        # self.label8 = tk.Label(self.stream_cipher_frame)
-        # self.label8.configure(text='state')
-        # self.label8.pack(side='left')
-        # self.stream_cipher_state = tk.Entry(self.stream_cipher_frame)
-        # self.stream_cipher_state.configure(justify='center', width='15')
-        # self.stream_cipher_state.delete(0, 'end')
-        # self.stream_cipher_state.insert(0, '6')
-        # self.stream_cipher_state.pack(fill='y', padx='10', side='left')
-        # self.button2 = tk.Button(self.stream_cipher_frame)
-        # self.button2.configure(text='Run')
-        # self.button2.pack(fill='y', ipadx='10', side='left')
-        # self.button2.configure(command=self.synchronous_stream_cipher_run)
-        # self.stream_cipher_frame.configure(height='200', width='200')
-        # self.stream_cipher_frame.pack(anchor='w', padx='20', pady='10', side='top')
-
-        self.frame11 = tk.Frame(self.main_window)
-        self.label27 = tk.Label(self.frame11)
-        self.label27.configure(font='{Bahnschrift SemiLight SemiConde} 16 {}', text='Output   ')
-        self.label27.pack(side='left')
-        self.code_output = ttk.Entry(self.frame11)
-        self.code_output.configure(width='80')
-        self.code_output.pack(anchor='center', fill='y', side='right')
-        self.frame11.configure(height='200', width='200')
-        self.frame11.pack(anchor='w', padx='20', pady='50', side='top')
-        self.main_window.configure(height='200', width='200')
-        self.main_window.geometry('640x500')
-
-        # Main widget
-        self.mainwindow = self.main_window
+        self.builder = builder = pygubu.Builder()
+        builder.add_resource_path(PROJECT_PATH)
+        builder.add_from_file(PROJECT_UI)
+        self.mainwindow = builder.get_object('main_window', master)
+        builder.connect_callbacks(self)
+        self.builder.get_variable('p_var').set(7)
+        self.builder.get_variable('q_var').set(11)
 
     def run(self):
         self.mainwindow.mainloop()
 
-    def insert_output_code(self, code):
-        self.code_output.delete(0, 'end')
-        self.code_output.insert(0, code)
+    def generate_public_private_key_call(self):
+        p = int(self.builder.get_variable('p_var').get())
+        q = int(self.builder.get_variable('q_var').get())
+        generate_public_private_key(p, q)
+        self.insert_output_text('Generated keys')
+        self.message_popup('Generated keys', 'Generated keys')
+
+    def encrypt_message_call(self):
+        pass
+
+    def decrypt_message_call(self):
+        pass
+
+    def select_file_call(self):
+        filename = filedialog.askopenfilename(title='Select an image')
+        self.insert_output_text(filename)
+
+        pass
+
+    def encrypt_file_call(self):
+        pass
+
+    def decrypt_file_call(self):
+        pass
+
+    def insert_output_text(self, text):
+        print(text)
+        self.builder.get_variable('output_var').set(text)
 
     def message_popup(self, title, text, type_message='info'):
         if type_message == 'info':
             tk.messagebox.showinfo(title, text)
         elif type_message == 'warning':
-            tkinter.messagebox.showwarning(title, text)
+            tk.messagebox.showwarning(title, text)
         elif type_message == 'error':
             tk.messagebox.showerror(title, text)
 
-    def rail_fence_run(self):
-        code_input = self.code_input.get()
-        if not code_input:
-            self.message_popup('Błąd', 'Nie wprowadzono kodu!', 'warning')
-            return
-        rail_fence_n = self.rail_fence_n_input.get()
-        if self.__tkvar.get() == 'Encrypt':
-            self.insert_output_code(RF1(code_input, int(rail_fence_n)))
-        else:
-            self.insert_output_code(RF2(code_input, int(rail_fence_n)))
 
-    def matrix_conversion1_run(self):
-        code_input = self.code_input.get()
-        if not code_input:
-            self.message_popup('Błąd', 'Nie wprowadzono kodu!', 'warning')
-            return
-
-        d = int(self.matrix_conversion_a_d_input.get())
-        coding_key = self.matrix_conversion_a_key_input.get()
-        coding_key = [int(s) for s in coding_key.split('-')]
-
-        if len(coding_key) != d:
-            self.message_popup('Błąd', 'Ilość kolumn w kluczu musi się równać d!', 'error')
-            return
-        for col in coding_key:
-            if col > d:
-                self.message_popup('Błąd', f'Liczby w kluczu nie mogą być większe od d! ({col} > {d})', 'error')
-                return
-        
-        if self.__tkvar.get() == 'Encrypt':
-            self.insert_output_code(PM(code_input, d=d, key=coding_key))
-        else:
-            self.insert_output_code(PM_decrypt(code_input, d=d, key=coding_key))
-
-    def matrix_conversion2_run(self):
-        code_input = self.code_input.get()
-        if not code_input:
-            self.message_popup('Błąd', 'Nie wprowadzono kodu!', 'warning')
-            return
-
-        coding_key = self.matrix_conversion_a_key_input.get()
-        if self.__tkvar.get() == 'Encrypt':
-            self.insert_output_code(PM2(code_input, coding_key))
-        else:
-            self.insert_output_code(PM2_decrypt(code_input, coding_key))
-
-    def random_generator_run(self):
-        input_key = self.entry1.get()
-        self.insert_output_code(random_generator_num(input_key))
-
-    def synchronous_stream_cipher_run(self):
-        input_code = self.code_input.get()
-        input_key = self.stream_cipher_key.get()
-        input_state = self.stream_cipher_state.get()
-        result = synchronous_stream_cipher(input_code, input_key, int(input_state))
-        self.insert_output_code(result)
-
-        
 
 if __name__ == '__main__':
-    app = AlgorytmyGuiApp()
+    app = RsaAlgorytmGuiApp()
     app.run()
