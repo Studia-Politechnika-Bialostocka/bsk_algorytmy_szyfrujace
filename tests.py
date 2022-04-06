82ec8f2b3fc766abf568326a51dfebbe4ba50dea
mateuszbialowas <mateusz.bialowas2@gmail.com>
2022-04-06 12:35:40 +0200

    backup

--- tests.py
+++ tests.py
@@ -1,40 +1,15 @@
 import unittest
 
 from algorytmy_impl import synchronous_stream_cipher
-from rsa_implementation import generate_rsa_keys, keysgen, numencrypt
+from rsa_implementation import makeKeyFiles
 
 
 class TestCrypt(unittest.TestCase):
-    def test_RSA(self):
-        p = 7
-        q = 11
-        actual = generate_rsa_keys(p, q)
-        expected = (77, 65537, 53)
+
+
+    def test_makeKeyFiles(self):
+        file_name = "RSA_demo"
+        key_size = 8
+        actual = makeKeyFiles(file_name, key_size)
+        expected = True
         self.assertEqual(actual, expected)
-
-        pass
-
-
-    def test_keysgen(self):
-        p = 7
-        q = 11
-        actual = keysgen(p, q)
-        expected = {'priv': (53, 77), 'pub': (35537, 77)}
-        self.assertEqual(actual, expected)
-
-    def test_numencrypt_encrypt(self):
-        priv = (720926705, 982634309)
-        pub = (35537, 982634309)
-        message = 80087
-        actual = numencrypt(message, priv)
-        expected = 539186383
-        self.assertEqual(actual, expected)
-
-    # def test_numencrypt_decrypt(self):
-    #     priv = (720926705, 982634309)
-    #     pub = (35537, 982634309)
-    #     message = 895887933
-    #     actual = numencrypt(message, pub)
-    #     expected = 1323123123
-    #     self.assertEqual(actual, expected)
-
