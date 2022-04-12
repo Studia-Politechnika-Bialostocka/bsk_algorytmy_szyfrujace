9adb2ffb66b9b984eaab393b574cb2456fe9c226
mateuszbialowas <mateusz.bialowas2@gmail.com>
2022-04-12 15:01:57 +0200

    Working encrypt and decrypt file.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import tkinter.ttk as ttk
 from tkinter import filedialog
 import pygubu
 
-from rsa_implementation import generate_public_private_key, encrypt_rsa, decrypt_rsa
+from rsa_implementation import generate_public_private_key, encrypt_rsa, decrypt_rsa, create_file
 
 PROJECT_PATH = pathlib.Path(__file__).parent
 PROJECT_UI = PROJECT_PATH / "rsa_algorytm_gui.ui"
@@ -27,8 +27,10 @@ class RsaAlgorytmGuiApp:
         builder.add_from_file(PROJECT_UI)
         self.mainwindow = builder.get_object('main_window', master)
         builder.connect_callbacks(self)
-        self.builder.get_variable('p_var').set(7)
-        self.builder.get_variable('q_var').set(11)
+        self.builder.get_variable('p_var').set(11)
+        self.builder.get_variable('q_var').set(13)
+        self.builder.get_variable('public_key_var').set('(7, 143)')
+        self.builder.get_variable('private_key_var').set('(223, 143)')
 
     def run(self):
         self.mainwindow.mainloop()
@@ -46,8 +48,6 @@ class RsaAlgorytmGuiApp:
         message = self.builder.get_variable('message_var').get()
         result = encrypt_rsa(message, key)
         self.insert_output_text(result)
-
-
 
     def decrypt_message_call(self):
         private_key = self.builder.get_variable('private_key_var').get()
@@ -69,10 +69,36 @@ class RsaAlgorytmGuiApp:
             message_popup('No file selected', 'No file selected')
             return
         else:
-            encrypt_file(self.filename)
+            public_key = self.builder.get_variable('public_key_var').get()
+            key = tuple([int(el) for el in public_key.replace('(', '').replace(')', '').split(',')])
+            f = open(self.filename, 'r')
+            message = f.read()
+            f.close()
+            result = encrypt_rsa(message, key)
+            # convert list to string
+            result = ' '.join([str(el) for el in result])
+            self.insert_output_text(result)
+            create_file(result, "encrypted_message.txt")
             message_popup('Encrypted file', 'Encrypted file')
 
     def decrypt_file_call(self):
+        if self.filename is None:
+            message_popup('No file selected', 'No file selected')
+            return
+        else:
+            private_key = self.builder.get_variable('private_key_var').get()
+            key = tuple([int(el) for el in private_key.replace('(', '').replace(')', '').split(',')])
+            f = open(self.filename, 'r')
+            message = f.read()
+            message = [int(el) for el in message.split(" ")]
+            f.close()
+            result = decrypt_rsa(message, key)
+            self.insert_output_text(result)
+            create_file(result, "decrypted_message.txt")
+            message_popup('Decrypted file', 'Decrypted file')
+
+
+
         pass
 
     def insert_output_text(self, text):
