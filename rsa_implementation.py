37e1819fa4b3be2c28146a5c0dcb8fe199f677b6
mateuszbialowas <mateusz.bialowas2@gmail.com>
2022-04-12 11:23:12 +0200

    Methods necessary to encrypt, decrypt file.

--- rsa_implementation.py
+++ rsa_implementation.py
@@ -1,2 +1,15 @@
 def generate_public_private_key(p, q):
     print(p, q)
+
+def encrypt_file(file_name):
+    f = open(file_name, 'r')
+    file_content = f.read()
+    f.close()
+    print(file_content)
+    create_encrypted_file(file_content)
+
+
+def create_encrypted_file(content):
+    f = open('encrypted_message.txt', 'w')
+    f.write(content)
+    f.close()
