1845ff09431339012c32907ad7f2a2334bf4ca18
Sylwia Mościcka <sysazeszkoly@gmail.com>
2022-04-24 12:44:00 +0200

    add generate public and private keys

--- tests.py
+++ tests.py
@@ -1,12 +1,11 @@
 import unittest
 
-from rsa_implementation import encrypt_rsa, decrypt_rsa
+from rsa_implementation import encrypt_rsa, decrypt_rsa, gcd, extended_gcd
 
 
 class TestCrypt(unittest.TestCase):
-
     def test_encrypt(self):
-        text = 'hello'
+        text = "hello"
         enc_key = (7, 143)
         actual = encrypt_rsa(text, enc_key)
         expected = [91, 62, 4, 4, 45]
@@ -16,5 +15,19 @@ class TestCrypt(unittest.TestCase):
         text = [91, 62, 4, 4, 45]
         dec_key = (223, 143)
         actual = decrypt_rsa(text, dec_key)
-        expected = 'hello'
+        expected = "hello"
+        self.assertEqual(actual, expected)
+
+    def test_gcd(self):
+        a = 8
+        b = 12
+        actual = gcd(a, b)
+        expected = 4
+        self.assertEqual(actual, expected)
+
+    def test_extended_gcd(self):
+        a = 8
+        b = 12
+        actual = extended_gcd(a, b)
+        expected = (4, -1, 1)
         self.assertEqual(actual, expected)
